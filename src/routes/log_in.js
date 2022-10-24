a6ec7f202f90b43af0ba4e00c01119bea374b229
Alex Liang <2alexliang@gmail.com>
2022-10-24 15:24:13 -0700

    Update log_in.js

--- src/routes/log_in.js
+++ src/routes/log_in.js
@@ -4,7 +4,7 @@ import "../assets/style.css"
 const client_id = '27240f6fd5374a14bd84f3598ed0725c';
 const auth_endpoint = 'https://accounts.spotify.com/authorize';
 const redirect_url = 'http://localhost:3000/home';
-const scope = 'user-read-email user-read-private user-top-read';
+const scope = 'user-read-private user-read-email';
 
 /* function to extract authorization token from URL */
 export const getTokenFromUrl = () => {
