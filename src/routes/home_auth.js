01f47f967a9622137a2173c561aac179ab6e84ca
Alex Liang <2alexliang@gmail.com>
2022-10-26 15:24:07 -0700

    Update home_auth.js
    Added getTokenFromUrl. Created an useEffect to call getTokenFromUrl and to create object to store access token, expiring time, and token type.

--- src/routes/home_auth.js
+++ src/routes/home_auth.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+
+/* function to extract authorization token from URL */
+export const getTokenFromUrl = (hash) => {
+  const accessToken = hash.substring(1);
+  const paramsInUrl =  accessToken.split('&');
+  const paramsSplit = paramsInUrl.reduce((accumulater, currentValue) => {
+      const [key, value] = currentValue.split('=');
+      accumulater[key] = value;
+      return accumulater;
+  }, {});
+  return paramsSplit;
+}
+
 export default function HomeAuth() {
+    useEffect(() => {
+      if(window.location.hash){
+        const object = getTokenFromUrl(window.location.hash);
+        console.log({ object });
+      }
+    });
+
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Welcome Home</h2>
