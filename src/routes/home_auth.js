964fc0dacc5c5aef1dbf1a25111054ae1ca49b3e
justbru <j.brunings2020@gmail.com>
2022-11-01 13:27:49 -0700

    added simple search bar that prints results to console

--- src/routes/home_auth.js
+++ src/routes/home_auth.js
@@ -1,39 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Router } from "react-router-dom";
 import { useHistory } from "react-router-dom"
+import axios from 'axios'
 
 /* function to extract authorization token from URL */
 export const getTokenFromUrl = (hash) => {
-  const accessToken = hash.substring(1);
-  const paramsInUrl =  accessToken.split('&');
-  const paramsSplit = paramsInUrl.reduce((accumulater, currentValue) => {
-      const [key, value] = currentValue.split('=');
-      accumulater[key] = value;
-      return accumulater;
-  }, {});
-  return paramsSplit;
+    const accessToken = hash.substring(1);
+    const paramsInUrl = accessToken.split('&');
+    const paramsSplit = paramsInUrl.reduce((accumulater, currentValue) => {
+        const [key, value] = currentValue.split('=');
+        accumulater[key] = value;
+        return accumulater;
+    }, {});
+    return paramsSplit;
 }
 
+const q = document.getElementById('query');
+
 export default function HomeAuth() {
+
+    const search_song = async () => {
+        const url = 'https://api.spotify.com/v1/search';
+        const searchQuery = document.getElementById('song_query').value;
+        const typeQuery = `type=track`;
+        const { data } = await axios.get(`${url}?q=${searchQuery}&${typeQuery}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.accessToken}`,
+            }
+        });
+        console.log(data);
+    }
+
     useEffect(() => {
-      if(window.location.hash){
-        const{
-          access_token,
-          expires_in,
-          token_type,
-        } = getTokenFromUrl(window.location.hash);
-        console.log({ access_token });
-        window.location.hash = "login=True";
-        localStorage.clear();
-        localStorage.setItem("accessToken", access_token);
-        localStorage.setItem("tokenType", token_type);
-        localStorage.setItem("expiresIn", expires_in);
-      }
+        if (window.location.hash) {
+            const {
+                access_token,
+                expires_in,
+                token_type,
+            } = getTokenFromUrl(window.location.hash);
+            console.log({ access_token });
+            window.location.hash = "login=True";
+            localStorage.clear();
+            localStorage.setItem("accessToken", access_token);
+            localStorage.setItem("tokenType", token_type);
+            localStorage.setItem("expiresIn", expires_in);
+        }
     });
 
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Welcome Home</h2>
-      </main>
+        <div>
+            <input type="search" id="song_query"
+                placeholder="Search..."></input>
+            <button onClick={search_song} style={{ backgroundColor: '#1DB954' }}>
+                Submit
+            </button>
+        </div>
     );
-  }
+}
