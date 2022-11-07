acf0c216cb062298ede8a9112f90c5b5040bbc1a
Alex Liang <2alexliang@gmail.com>
2022-11-07 11:48:16 -0800

    Update playlist_generator.js

--- src/routes/playlist_generator.js
+++ src/routes/playlist_generator.js
@@ -1,6 +1,6 @@
 //import { useEffect } from "react";
 //import { Router } from "react-router-dom";
-//import axios from 'axios';
+import axios from 'axios';
 import SpotifyWebApi from "spotify-web-api-js";
 var Spotify = require('spotify-web-api-js');
 
@@ -11,10 +11,21 @@ var api = new SpotifyWebApi();
 api.setAccessToken(localStorage.getItem('accessToken'));
 
 // Function to parse through each user saved song
-export const parseTracks = async () => {
-    // Get track id using search_song and store them into db?
+export const getSongByTempo = async () => {
+    // Get track id using liked_songs
+    const data = liked_songs();
+    const len = Object.keys(data.items).length;
+
+    // Can get track id using items[n].track.id
     // For each song id, getAudioAnalysis and extract tempo
     // If tempo is within range of user input, then put into list
+    for(i = 0; i < len; i++){
+        const tempo = getAudioAnalysis(data.items[i].track.id);
+        if(tempo == input_tempo){
+            //Get data.items[i].track.id and put into playlist
+            return;
+        }
+    }
 }
 
 // Function to get track tempo
